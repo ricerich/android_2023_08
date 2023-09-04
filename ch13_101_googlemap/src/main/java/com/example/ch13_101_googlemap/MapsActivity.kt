42cdb76da14a1eb035812b2847b0255b6562e2e1
ricerich <ricerich@gmail.com>
2023-09-04 14:24:27 +0900

    13장 지도 문제까지

--- ch13_101_googlemap/src/main/java/com/example/ch13_101_googlemap/MapsActivity.kt
+++ ch13_101_googlemap/src/main/java/com/example/ch13_101_googlemap/MapsActivity.kt
@@ -2,6 +2,8 @@ package com.example.ch13_101_googlemap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,11 +12,16 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.ch13_101_googlemap.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.GroundOverlayOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+
+    lateinit var videoMark: GroundOverlayOptions//추가
+    
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,11 +46,60 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * installed Google Play services and returned to the app.
      */
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+//        mMap = googleMap
+//
+//        // Add a marker in Sydney and move the camera
+//        val sydney = LatLng(-34.0, 151.0)
+//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap = googleMap
+        mMap.uiSettings.isZoomControlsEnabled = true
+        mMap.setOnMapClickListener { point ->
+            videoMark = GroundOverlayOptions().image(
+                BitmapDescriptorFactory.fromResource(R.drawable.presence_video_busy))
+                .position(point, 100f, 100f)
+            mMap.addGroundOverlay(videoMark)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menu.add(0, 1, 0, "위성 지도")
+        menu.add(0, 2, 0, "일반 지도")
+        val sMenu = menu.addSubMenu("유명장소 바로가기 >>")
+        sMenu.add(0, 3, 0, "정동진")
+        sMenu.add(0, 4, 0, "해운대")
+        sMenu.add(0, 5, 0, "땅끝마을")
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            1 -> {
+                mMap.mapType = GoogleMap.MAP_TYPE_HYBRID
+                return true
+            }
+            2 -> {
+                mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+                return true
+            }
+            3 -> {
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(
+                    37.689254, 129.033051), 15f))
+                return true
+            }
+            4 -> {
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(
+                    35.159003, 129.161882), 15f))
+                return true
+            }
+            5 -> {
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(
+                    34.301472, 126.524048), 15f))
+                return true
+            }
+        }
+        return false
     }
 }
